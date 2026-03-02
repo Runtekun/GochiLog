986559e38995d19c9c2f87cf7c2c173947db4f79
工藤 <kudo@kudounoMacBook-Air.local>
2026-03-03 05:10:37 +0900

    rubocop修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,6 @@ class ApplicationController < ActionController::Base
 
   # nameカラムをユーザー登録の際に許可
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ])
   end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   validates :name, length: { maximum: 30 }, presence: true, uniqueness: true
   validates :email, length: { maximum: 255 }, presence: true, uniqueness: true
   validates :password, length: { minimum: 6 }, presence: true
