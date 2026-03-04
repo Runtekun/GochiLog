6f1b613d408885a68f9a11fb4d70aa49c66afca7
工藤 <kudo@kudounoMacBook-Air.local>
2026-03-04 23:10:00 +0900

    ビューを実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :reviews, dependent: :destroy
+  has_one_attached :avatar
 
   validates :name, length: { maximum: 30 }, presence: true, uniqueness: true
   validates :email, length: { maximum: 255 }, presence: true, uniqueness: true
