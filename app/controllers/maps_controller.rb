6d35aa89cd6087b5396d648adbcfefaeff0215cc
工藤 <kudo@kudounoMacBook-Air.local>
2026-03-06 00:33:40 +0900

    mapコントローラーを実装

---
+++ app/controllers/maps_controller.rb
@@ -0,0 +1,17 @@
+class MapsController < ApplicationController
+
+  def index
+    # すべての店舗とその店舗に関連するレビューを取得し、JSON形式で返す
+    @shops = Shop.includes(:reviews).all
+    @shops_json = @shops.map do |shop|
+      review = shop.reviews.first
+      {
+        id: shop.id,
+        name: shop.name,
+        latitude: shop.latitude,
+        longitude: shop.longitude,
+        review_id: review&.id
+      }
+    end.to_json
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "maps/index"
   get "reviews/index"
   get "reviews/show"
   get "reviews/new"
