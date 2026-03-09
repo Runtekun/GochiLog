e34ddd9cfff182c8232625785bbf8a31ed14969a
工藤 <kudo@kudounoMacBook-Air.local>
2026-03-10 00:21:24 +0900

    パスワードなしで編集できるようにコントローラーにメソッドを実装

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,27 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update, :edit]
+
   protected
 
+  # nameカラムをユーザー登録の際に許可
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  # name, bio, avatarカラムをユーザー更新の際に許可
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :bio, :avatar])
+  end
+
+  # ユーザーがプロフィールを更新する際に、現在のパスワードを要求しないようにするためのメソッド
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
+
+  # ユーザーがプロフィールを更新する際に、現在のパスワードを要求するかどうかを判断するメソッド
   def after_sign_up_path_for(resource)
     reviews_path
   end
