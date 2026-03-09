04cda465b9f3932426f85096dd58e1f61cedd0d5
工藤 <kudo@kudounoMacBook-Air.local>
2026-03-09 14:40:43 +0900

    fix: コントローラーテストを修正

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,6 +8,7 @@ module ActiveSupport
     parallelize(workers: :number_of_processors)
 
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+    include Devise::Test::IntegrationHelpers
     fixtures :all
 
     # Add more helper methods to be used by all tests here...
