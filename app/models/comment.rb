5a57cb3ab7dec19f14e11b67b2ef2f2ef5c46331
工藤 <kudo@kudounoMacBook-Air.local>
2026-04-03 03:00:54 +0900

    feat: 各モデルにいいね・コメント・フォロー時の通知生成を追加

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,20 @@ class Comment < ApplicationRecord
   belongs_to :review
 
   validates :body, presence: true
+
+  # コメントが作成されたときに通知を作成するメソッド
+  after_create :create_notification
+
+  private
+
+  def create_notification
+    return if user == review.user
+    Notification.create(
+      visitor_id: user_id,
+      visited_id: review.user_id,
+      review_id: review_id,
+      comment_id: id,
+      action: "comment"
+    )
+  end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -2,4 +2,19 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :review
   validates :user_id, uniqueness: { scope: :review_id }
+
+  # いいねが作成されたときに通知を作成するメソッド
+  after_create :create_notification
+
+  private
+
+  def create_notification
+    return if user == review.user
+    Notification.create(
+      visitor_id: user_id,
+      visited_id: review.user_id,
+      review_id: review_id,
+      action: "like"
+    )
+  end
 end
