313fdcaa8bd84d7b87eb1975515e88d3374ad5cc
工藤 <kudo@kudounoMacBook-Air.local>
2026-05-01 23:55:22 +0900

    fix: after_create_commitコールバックの競合を解消

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -4,10 +4,25 @@ class Relationship < ApplicationRecord
 
   validates :follower_id, uniqueness: { scope: :followed_id }
 
-  # フォローが作成されたときに通知を作成するメソッド
-  after_create :create_notification
+  # フォローが作成・削除されたときに通知作成とリアルタイム更新を行う
+  after_create_commit :create_notification
+  after_destroy_commit :broadcast_follow_count
 
   private
+
+  # フォロワー数・フォロー中数をリアルタイムでブロードキャストする
+  def broadcast_follow_count
+    FollowsChannel.broadcast_to(followed, {
+      user_id: followed_id,
+      followers_count: followed.followers.count,
+      following_count: followed.following.count
+    })
+    FollowsChannel.broadcast_to(follower, {
+      user_id: follower_id,
+      followers_count: follower.followers.count,
+      following_count: follower.following.count
+    })
+  end
 
   # フォローの通知を作成するメソッド
   def create_notification
